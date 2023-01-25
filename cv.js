fc7d1d7e7c15bc62b560ca550fea978b874eb93f
IrinaGvs <123343249+IrinaGvs@users.noreply.github.com>
2023-01-25 12:01:25 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,14 @@
+document.getElementById('boton').onclick=function(){
+    console.log ("Capturamos el evento click")
+    document.getElementById("demo").innerHTML="(925) 519-5308";
+}
+
+document.getElementById('boton2').onclick=function(){
+    console.log ("Capturamos el evento click")
+    document.getElementById("demo").innerHTML="deann.walters@example.com"
+}
+
+document.getElementById('boton3').onclick=function(){
+    console.log ("Capturamos el evento click")
+    document.getElementById("demo").innerHTML="7593 Homestead Rd"
+}
